1058fd7084d02d3c3dc97a90bcef50daf445a058
juansosun <jsordonezs@unal.edu.co>
2023-02-09 09:46:53 -0500

    Update the flux code since no change in the numeration is now needed

--- flux.py
+++ flux.py
@@ -1,25 +1,5 @@
 import numpy as np 
 
-def prepare_array_efficiency(array_efficiencies:list):
-    """
-    Since different numerations of the pads were used, at the end it is necessary to 
-    make them compatible.
-    """
-    array_efficiencies = np.array(array_efficiencies).reshape((8,8))
-    array_efficiencies = np.transpose(array_efficiencies)
-    
-    array_cols = [array_efficiencies.T[ii] for ii in range(0,8)]
-    array_efficiencies_aux = np.zeros((8,8))
-    for ii in range(0,8):
-        col = list(array_cols[ii])
-        col.reverse()
-        array_efficiencies_aux.T[ii] = col 
-    
-    array_efficiencies_aux = array_efficiencies_aux.reshape((1,64))
-    array_efficiencies_aux = [array_efficiencies_aux[0][ii] for ii in range(0,64)]
-
-    return array_efficiencies_aux
-
 def N_total(N_obs_pad:list,eff_pad:list)->float:
     """
     Final number of muons for all pads considering the efficiency correction.
@@ -33,11 +13,10 @@ def N_total(N_obs_pad:list,eff_pad:list)->float:
 
 def flux_total(A:float,dt:float,eff_dict:dict,N_obs_list:list)->float:
     """
-    Final flux, taking into account sensible area an time of measurent.
+    Final flux, taking into account sensible area and time of measurment.
     """
 
     eff_list = list(eff_dict.values())
-    eff_list = prepare_array_efficiency(eff_list)
 
     N_tot = N_total(N_obs_list,eff_list)
     
